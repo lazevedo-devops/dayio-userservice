799bfe8bde03ef0e7d0533a57864e5747dded572
Lucas Azevedo <lucas.azevedo@digibee.com>
2023-12-16 15:24:30 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# dayio-test
+# User Service API
+
+#### Description: This API is a sample API that returns a Hello World for User Service
+
+#### Install
+
+Commands HERE!
+
+#### Usage 
+
+Instructions HERE!
+
+
+
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.text());
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+  });
+
+app.get('/user', (req, res) => {
+    res.set('Content-Type', 'text/html');
+    res.status(200).send(
+        "Hello World User Service!"
+    );
+});
